65f74412d0b69756b250c8c01d29ec13b8750f7e
Eduardo Moreira <Du_du.mba@hotmail.com>
2023-02-06 15:16:25 -0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import colors from "colors";
 import connectDB from "./config/db.js";
-import productRoutes  from  "./routes/productRoutes.js"
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const app = express();
 app.use(cors());
@@ -16,11 +17,17 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.use('/api/products', productRoutes)
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
